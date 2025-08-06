5983ab642127f0227e489f8a21ff872eca23faa5
Abbas <Abbas@gamil.com>
2025-08-07 00:30:57 +0300

    Add Logger For Start plugin

--- src/main/java/com/Relxwe/npcPlugin/NpcPlugin.java
+++ src/main/java/com/Relxwe/npcPlugin/NpcPlugin.java
@@ -2,16 +2,26 @@ package com.Relxwe.npcPlugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public final class NpcPlugin extends JavaPlugin {
+
+    public Logger logger;
+
 
     @Override
     public void onEnable() {
+        logger.info("NpcPlugin has been enabled!");
         // Plugin startup logic
+        this.logger = this.getLogger();
+        // Register commands, events, etc.
 
     }
 
     @Override
     public void onDisable() {
+        logger.info("NpcPlugin has been disabled!");
         // Plugin shutdown logic
+        // Clean up resources, save data, etc.
     }
 }
