31e3e9e30f941d7f3609cd91bc8883f13525dad7
Abbas <Abbas@gamil.com>
2025-08-07 00:02:33 +0300

    Initial commit

---
+++ src/main/java/com/Relxwe/npcPlugin/NpcPlugin.java
@@ -0,0 +1,17 @@
+package com.Relxwe.npcPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NpcPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
